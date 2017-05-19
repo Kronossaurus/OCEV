565ef97f798de267e9a720060c4fbd9d750de977
Kronossaurus <brvictor.chess@hotmail.com>
2017-05-19 16:47:02 -0300

    Queens Checker

---
+++ queensChecker.py
@@ -0,0 +1,21 @@
+file 		= open("teste.txt", "r")
+n 			= file.readline()
+queens 		= file.	readline().split()
+conflict 	= []
+
+for i in queens:
+	for j in queens[queens.index(i)+1::]:
+		if i == j or abs(int(i)-int(j)) == abs(queens.index(i)-queens.index(j)):
+			# if queens.index(i)+1 not in conflict:
+			# 	conflict.append(queens.index(i)+1);
+			# if queens.index(j)+1 not in conflict:
+			# 	conflict.append(queens.index(j)+1);
+			if i not in conflict:
+				conflict.append(i);
+			if j not in conflict:
+				conflict.append(j);
+			
+if len(conflict) > 0:
+	print conflict
+else:
+	print "Sucesso"
